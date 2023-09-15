7974e962eec29a83ea0082557a6cb63388bd6a0f
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2023-09-15 11:13:01 +0530

    lab

--- Python_Work/Assignment/3.py
+++ Python_Work/Assignment/3.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 l=[7, 4, 1, 17, 19, 17, 1, 5, 3, 10]
 l=list(set(l))
 # print(l)
@@ -6,3 +7,11 @@ for i in range (0,len(l)):
         if (l[i]*l[j])%2!=0 :
             print(f'({l[i]}, {l[j]})')
 # print(l)
+=======
+import random
+l=[]
+for i in range(0,10):
+    x= random.randint(1,20)
+    l.append(x)
+print(l)
+>>>>>>> Stashed changes
