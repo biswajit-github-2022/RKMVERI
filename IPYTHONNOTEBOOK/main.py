73dda471ca70c9c7ff9880ee63353c1dce7a03f3
biswajit-github-2022 <biswajitranaplanet@gmail.com>
2023-09-29 12:43:57 +0530

    Lab

--- IPYTHONNOTEBOOK/main.py
+++ IPYTHONNOTEBOOK/main.py
@@ -1,15 +1,17 @@
-import time 
+# import time 
 
 
-def deco_func(f):
-    def inner_func():
-        t1=time.time()
-        f()
-        t2=time.time()
-        return f"time taken to exc f ={t2-t1}"
-    return inner_func
+# def deco_func(f):
+#     def inner_func():
+#         t1=time.time()
+#         f()
+#         t2=time.time()
+#         return f"time taken to exc f ={t2-t1}"
+#     return inner_func
 
 
-@deco_func
-def f():
-    return "hi"
+# @deco_func
+# def f():
+#     return "hi"
+
+
